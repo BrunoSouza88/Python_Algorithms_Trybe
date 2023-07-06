2daf55b3ff0d82dc7c1b36616ba60417f1d91cc1
BrunoSouza <desouza88@hotmail.com>
2023-07-06 15:28:49 -0300

    feat: Req03: is_palindrome_recursive implemented

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+    if low_index >= high_index:
+        return True
+    first_letter = word[low_index]
+    last_letter = word[high_index]
+    if first_letter == last_letter:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    else:
+        return False
