75842139a75108199e4d58bca51bdc33091b9b20
BrunoSouza <desouza88@hotmail.com>
2023-07-06 18:12:44 -0300

    fix: Req02

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -11,4 +11,3 @@ def test_encrypt_message():
     assert encrypt_message('hello world', 20) == 'dlrow olleh'
     assert encrypt_message('hello world', 5) == 'olleh_dlrow '
     assert encrypt_message('hello world', 4) == 'dlrow o_lleh'
-    assert encrypt_message('hello world', 1) == 'hello world'
