10ac1ab7cf5b21fd4b6a146df0641c0b02aefdde
BrunoSouza <desouza88@hotmail.com>
2023-07-07 17:38:20 -0300

    feat: Req06: palindromes_interative implemented

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,14 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+    word = word.lower()
+    if len(word) == 1:
+        return True
+    left = 0
+    right = len(word) - 1
+    while left < right:
+        if word[left] != word[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
